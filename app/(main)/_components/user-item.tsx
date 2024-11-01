a8050ae482dba2e0e1b1f79f0e65f1d28ceccc1b
DomDeChiera <dom@dechiera.com>
2024-11-01 23:16:07 +0800

    Add user dropdown menu

--- app/(main)/_components/user-item.tsx
+++ app/(main)/_components/user-item.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ChevronsLeftRight } from "lucide-react";
-import { useUser } from "@clerk/clerk-react";
+import { SignOutButton, useUser } from "@clerk/clerk-react";
 
 import {
     Avatar,
@@ -57,6 +57,12 @@ export const UserItem = () => {
                         </div>
                     </div>
                 </div>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem asChild className="w-full cursor-pointer text-muted-foreground">
+                    <SignOutButton>
+                        Log out
+                    </SignOutButton>
+                </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
     )
